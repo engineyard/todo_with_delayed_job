45035ca93f608b13cdabe24eea01f21fda33f69f
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-14 19:15:31 -0700

    Fix variable names for node/json

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,11 +1,12 @@
-# Runs application cookbooks
 
-custom_json = json.dup
-custom_json['run_list'] = 'recipe[main]'
+# Set up a chef 0.10 dna.json file (for stack-v1 + stack-v2)
+# TODO does this run on non-app-master/solo?
+custom_json = node.dup
+node['run_list'] = 'recipe[main]'
 File.open(custom_dna, 'w') do |f|
   f.puts JSON.pretty_generate(custom_json)
   f.chmod(0600)
 end
 
-
+# Runs application cookbooks
 run "cd #{latest_release}; sudo bundle exec chef-solo -c #{latest_release}/deploy/solo.rb -j /etc/chef-custom/dna.json"
