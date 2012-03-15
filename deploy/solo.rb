7508446f39f9fc4f7a30755f7be1794df33c4920
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-14 19:44:06 -0700

    try giving solo.rb many cookbook_paths

--- deploy/solo.rb
+++ deploy/solo.rb
@@ -1,8 +1,16 @@
 base_dir = File.dirname(__FILE__)
 node = Yajl::Parser.new.parse(File.new("/etc/chef-custom/dna.json", "r"))
 
+cookbook_paths = ["#{base_dir}/cookbooks"]
+$LOAD_PATH.each do |path|
+  base_path = path.gsub(%r{/lib$},'')
+  if File.exist?(File.join(base_path, 'metadata.json'))
+    cookbook_paths << File.dirname(base_path)
+  end
+end
+
 file_store_path   base_dir
 file_cache_path   base_dir
-cookbook_path     "#{base_dir}/cookbooks"
+cookbook_path     cookbook_paths
 role_path         "#{base_dir}/roles"
 node_name         node["engineyard"]["this"]
