0eff154faf761aaf738dfc66f9fd8fbdc85a6650
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-14 20:23:01 -0700

    Revert "Trying to add discovered recipes to the run_list via solo.rb"
    This reverts commit 9c2108f7d099a2578466ec8be8ee404f1f8f60e7.

--- deploy/solo.rb
+++ deploy/solo.rb
@@ -9,20 +9,16 @@ cookbook_path     cookbooks_path
 role_path         "#{base_dir}/roles"
 node_name         node["engineyard"]["this"]
 
-node["run_list"] = (node["run_list"].is_a?(Array) ? node["run_list"] : [node["run_list"]])
-
 require "fileutils"
 $LOAD_PATH.each do |path|
   recipe_path = path.gsub(%r{/lib$},'')
   if File.exist?(metadata_path = File.join(recipe_path, 'metadata.json'))
     metadata = Yajl::Parser.new.parse(File.new(metadata_path, "r"))
-    unless name = metadata["name"]
-      puts "ERROR: Recipe has no 'name' in metadata.json (#{cookbooks_path})"
-    else
+    if metadata["name"]
       puts "Unpacking #{metadata["name"]} recipe into cookbooks from #{cookbooks_path}"
-      FileUtils.cp_r(recipe_path, File.join(cookbooks_path, name))
-
-      node["run_list"] << "recipe[#{name}]"
+      FileUtils.cp_r(recipe_path, File.join(cookbooks_path, metadata["name"]))
+    else
+      puts "ERROR: Recipe has no 'name' in metadata.json (#{cookbooks_path})"
     end
   end
 end
