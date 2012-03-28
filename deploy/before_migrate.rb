d89bbd5eb6ed195206333637502d9d55a50caa77
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-27 17:18:21 -0700

    Show stacktrace on chef error

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,4 @@
-
 # Set up a chef 0.10 dna.json file (for stack-v1 + stack-v2)
-# TODO does this run on non-app-master/solo?
 custom_json = node.dup
 custom_json['run_list'] = 'recipe[main]'
 File.open("/etc/chef-custom/dna.json", 'w') do |f|
@@ -10,3 +8,5 @@ end
 
 # Runs application cookbooks
 run "cd #{latest_release}; sudo bundle exec chef-solo -c #{latest_release}/deploy/solo.rb -j /etc/chef-custom/dna.json"
+
+run "cat #{latest_release}/deploy/chef-stacktrace.out 2> /dev/null || echox 'Chef run successful'"
